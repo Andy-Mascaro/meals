75bfd173e998049942c33db488716283170490a4
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-31 17:59:44 -0800

    added const

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 //import { renderStat, renderGame } from './utils.js';
 // let state
 const form = document.getElementById('add-ingredient');
+const foodList = document.getElementById('food-list');
+const mealList = document.getElementById('meal-list');
+const remove = document.getElementById('remove');
+const save = document.getElementById('save-meal');
 
-// form.addEventListener('submit', (e) =>{
-//     e.preventDefault();
-//     const formData = new FormData(form);
+let ingredient = [];
+let quantity = [];
+let measurement = [];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const meal = {
+        ingredient: formData.get('ingredient'),
+        quantity: formData.get('quantity'),
+        measurment: formData.get('measurment'),
+
+    };
+
+    meal.push(meal);
+    renderStats();
+    form.requestFullscreen();
     
-// });
+    
+});
 
 // set event listeners 
   // get user input
