4e08d7d8cb9855d7500d45e2a83e86085291973c
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-31 17:27:06 -0800

    tested my form and button with console log

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // import functions and grab DOM elements
-
+//import { renderStat, renderGame } from './utils.js';
 // let state
+const form = document.getElementById('add-ingredient');
+
+// form.addEventListener('submit', (e) =>{
+//     e.preventDefault();
+//     const formData = new FormData(form);
+    
+// });
 
 // set event listeners 
   // get user input
